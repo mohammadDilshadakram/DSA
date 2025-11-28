3d947cb47b2d969a4f6e887f393749442731b73e
mohammadDilshadakram <mddilshadakram123@gmail.com>
2025-11-28 15:09:35 +0530

    heao

--- heap/insertion.java
+++ heap/insertion.java
@@ -23,6 +23,37 @@ public class insertion {
                 System.out.println(al.get(i));
             }
         }
+
+        public static int delete(){
+            int data=al.get(0);
+            int temp=al.get(0);
+            al.set(0, al.get(al.size()-1));
+            al.set(al.size()-1, temp);
+            al.remove(al.size()-1);
+            heapify(0);
+            return data;
+        }
+
+        public static void heapify(int i){
+            int maxIdx=i;
+            int left=(2*i)+1;
+            int right=(2*i)+2;
+            if (left<al.size() && al.get(left)>al.get(i)) {
+                 maxIdx=left;
+            }
+            if (right<al.size() && al.get(right)>al.get(i)) {
+                maxIdx=right;
+            }
+            if (maxIdx!=i) {
+                int temp=al.get(i);
+                al.set(i, al.get(maxIdx));
+                al.set(maxIdx, temp);
+                heapify(maxIdx);
+            }
+            
+
+        }
+
     } 
     public static void main(String[] args) {
         Heap h1=new Heap();
@@ -32,6 +63,9 @@ public class insertion {
         h1.insert(1);
         h1.insert(0);
         h1.insert(90);
+        h1.display();
+        System.out.println("After deletion");
+        h1.delete();
         h1.display();
 
      
